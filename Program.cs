7eb882a0f1c5ae8ea851c142250a0ea2c4087e20
Berke ÖDEN <53332511+berkeodn@users.noreply.github.com>
2022-01-26 14:12:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+// While 
+// 1 den başlayarak consoldan girilen sayıya kadar sayı dahil ortalama hesaplayıp konsola yazdıran program.
+Console.Write("Lütfen bir sayı giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac ++;
+}
+Console.WriteLine(toplam/sayi);
+
+// "a" dan "z" ye kadar tüm harfleri consola yazdıralım.
+char character = 'a';
+while (character < 'z')
+{
+    Console.Write(character);
+    character ++;
+}
+
+Console.WriteLine("******* Foreach *******");
+string [] arabalar = {"BMW", "Toyota", "Ford", "Mercedes"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
